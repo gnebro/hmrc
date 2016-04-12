9a982949019733ffee84aeb452850664285a908e
Lorenzo Ansaloni <lorenzo.ansaloni@monitise.com>
2016-04-12 16:34:48 +0100

    First draft
    Change-Id: Ie904e16e709a49770b8b62949010c9a9f8f3d57b

---
+++ src/main/scala/org/hmrc/checkout/CheckoutSystem.scala
@@ -0,0 +1,8 @@
+package org.hmrc.checkout
+
+/**
+  * Created by AnsaloniL on 12/04/2016.
+  */
+class CheckoutSystem {
+
+}
---
+++ src/test/scala/org/hmrc/checkout/CheckoutSystemSpec.scala
@@ -0,0 +1,24 @@
+package org.hmrc.checkout
+
+import org.scalatest.{BeforeAndAfter, Matchers, FlatSpec}
+
+/**
+  * Created by AnsaloniL on 12/04/2016.
+  */
+class CheckoutSystemSpec extends FlatSpec with Matchers with BeforeAndAfter {
+
+  import TestFixture._
+
+  before {
+
+  }
+
+  "checkOutSystem " should " do something" in {
+
+  }
+
+}
+
+object TestFixture {
+
+}
